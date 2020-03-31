0a62e5c188ba4a203514c5c7876a575ac9291479
Jeff W <Jeff@fraction.rent>
2020-03-31 03:12:50 -0400

    Updates

---
+++ precache-manifest.1b1325f077d3b9df5b9948de08dfcaea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f1f3ee35e88be26522153a31093176",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f252d6796d2379620cb0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "701c4e5359205532f465",
+    "url": "/static/js/2.81737e79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.81737e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f252d6796d2379620cb0",
+    "url": "/static/js/main.86377204.chunk.js"
+  },
+  {
+    "revision": "8fce1b061d00c0dff0ee",
+    "url": "/static/js/runtime-main.b1fd9137.js"
+  }
+]);
